93cd604bb84adaa5a36b58359618d05e0c0d9c50
satyanarayan453 <74782403+satyanarayan453@users.noreply.github.com>
2022-08-28 12:09:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+let navItems_ul = document.getElementById("nav-list-itmes");
+let hamburg = document.getElementById("hamburg_menubar");
+let navLinkItems = document.getElementsByName("nav-item") ;
+function toogleNav(){
+    navItems_ul.classList.toggle("nav_links_flip");
+    hamburg.classList.toggle("cross-nav-burger");
+}
+hamburg.addEventListener("click" ,toogleNav);
+navLinkItems.forEach(function(link){
+    link.addEventListener("click", toogleNav)
+});
+let skillsContent = document.getElementById("skills");
+let experienceContent = document.getElementById("experience");
+let educationContent = document.getElementById("education");
+let skillsLink = document.getElementById("skills-link");
+let experienceLink = document.getElementById("experience-link");
+let educationLink = document.getElementById("education-link");
+function openExperienceTab(){
+    experienceContent.classList.add("active-tab-content");
+    experienceLink.classList.add("active-tab-link");
+
+    skillsContent.classList.remove("active-tab-content");
+    skillsLink.classList.remove("active-tab-link");
+    educationContent.classList.remove("active-tab-content");
+    educationLink.classList.remove("active-tab-link");
+}
+function openSkillsTab(){
+    skillsContent.classList.add("active-tab-content");
+    skillsLink.classList.add("active-tab-link");
+    
+    experienceContent.classList.remove("active-tab-content");
+    experienceLink.classList.remove("active-tab-link");
+    educationContent.classList.remove("active-tab-content");
+    educationLink.classList.remove("active-tab-link");
+}
+function openEducationTab(){
+    educationContent.classList.add("active-tab-content");
+    educationLink.classList.add("active-tab-link");
+    
+    skillsContent.classList.remove("active-tab-content");
+    skillsLink.classList.remove("active-tab-link");
+    experienceContent.classList.remove("active-tab-content");
+    experienceLink.classList.remove("active-tab-link");
+}
+//openExperienceTab()
